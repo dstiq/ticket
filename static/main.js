8609b7d35eac41b322b0ebc3b3514b2aeededa50
17293013 <D5i4m3a2s1>
2020-10-06 13:38:42 +0300

    change logo size

--- static/main.js
+++ static/main.js
@@ -1,19 +1,18 @@
 function getInfo() {
     var params = new URLSearchParams(location.search);
-    var ticket = params.get('ticket');
-    var operation = params.get('operation');
-    var dateTime = params.get('dateTime');
-    return [ticket, operation, dateTime];
+    var ticket = params.get('ticketCode');
+    var operation = params.get('operationName');
+    return [ticket, operation];
 }
 
 
 function drawInfo(params) {
     var ticketElem = document.getElementById("ticket");
     var operationElem = document.getElementById("operation");
-    var dateTimeElem = document.getElementById("dateTime");
+
     ticketElem.innerText = params[0] + "";
     operationElem.innerText = params[1] + "";
-    dateTimeElem.innerText = params[2] + "";
+
 }
 
 function main() {
